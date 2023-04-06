3f7321b3418b08bc8df7f284497581896553880e
donghee <jjj4435@naver.com>
2023-04-06 14:09:55 +0900

    Initial commit

---
+++ src/main/java/com/codestates/section4solo/Section4SoloApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.section4solo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4SoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section4SoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/section4solo/Section4SoloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.section4solo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4SoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
